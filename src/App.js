c9770dff7e34a1b713aacbb0ef00f5becd1e24db
Nikita <ivanovvnikita@mail.ru>
2022-11-08 21:03:38 +0500

    added modal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import postItem from "./components/PostItem";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -17,6 +18,7 @@ function App() {
         {id: 3, title: 'вв 3', body: 'яя'}
     ]);
     const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
     
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
@@ -31,6 +33,7 @@ function App() {
     
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
+        setModal(false);
     }
     
     const removePost = (post) => {
@@ -39,7 +42,12 @@ function App() {
     
     return (
         <div className='App'>
-            <PostForm create={createPost}/>
+            <MyButton style={{marginTop: 20}} onClick={() => setModal(true)}>
+                Создать пост
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
             <PostList remove={removePost} posts={sortedAnsSearchedPosts} title='Список постов 1'/>
--- src/components/UI/MyModal/MyModal.jsx
+++ src/components/UI/MyModal/MyModal.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import classes from './MyModal.module.css';
 
-const MyModal = () => {
+const MyModal = ({children, visible, setVisible}) => {
+    
+    const rootClasses = [classes.myModal];
+    
+    if (visible) {
+        rootClasses.push(classes.active);
+    }
+    
     return (
-        <div>
-            
-  </div>
+        <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+            <div className={classes.myModalContent} onClick={(e) => e.stopPropagation()}>
+                {children}
+            </div>
+        </div>
     );
 };
 
